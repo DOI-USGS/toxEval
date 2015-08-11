10271b4ca2f51e2e0e00b70e7762ee0489c2b94c
Laura DeCicco <ldecicco@usgs.gov>
2015-08-10 22:48:06 -0500

    Took out yLim reordered group.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -44,7 +44,7 @@ shinyUI(
                                   selected = 1),
                 tabsetPanel(
                   tabPanel("Visualizations",
-                           uiOutput("numControl1"),
+                           # uiOutput("numControl1"),
                            plotOutput("stackBarGroup"),
                            h4("All EARs"),
                            plotOutput("graphGroup")),
@@ -67,7 +67,7 @@ shinyUI(
                  tabsetPanel(
                    tabPanel("Visualizations",
                             htmlOutput("BoxHeader"),
-                            uiOutput("numControl2"),
+                            # uiOutput("numControl2"),
                             plotOutput("stackBar"),
                             h5("All EARs"),
                             plotOutput("graph")
