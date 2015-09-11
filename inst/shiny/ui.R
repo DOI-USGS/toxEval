7c29d3c5d632133c638cb666a40d604f77d311c9
Laura DeCicco <ldecicco@usgs.gov>
2015-09-11 15:47:56 -0500

    Undoing endpoint graphs, added disclaimer.

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -40,7 +40,7 @@ shinyUI(
             tabPanel("Annotation Summary",
                 htmlOutput("TableHeaderColumns"),
                 radioButtons("radioMaxGroup", label = "",inline = TRUE,
-                                  choices = list("Choice" = 1, "Chemical" = 2, "Class" = 3, "Endpoint" = 4), 
+                                  choices = list("Choice" = 1, "Chemical" = 2, "Class" = 3), 
                                   selected = 1),
                 tabsetPanel(
                   tabPanel("Visualizations",
@@ -61,7 +61,7 @@ shinyUI(
                  fluidRow(
                    column(5,uiOutput("groupControl")),
                    column(5,radioButtons("radio", label = "", inline = TRUE,
-                                       choices = list("Chemical" = 1, "Class" = 2, "EndPoint" = 3), 
+                                       choices = list("Chemical" = 1, "Class" = 2), 
                                        selected = 1))
                  ),
                  tabsetPanel(
@@ -82,6 +82,15 @@ shinyUI(
           )
         ),
       column(1)
+    ),
+    fluidRow(
+      column(1),
+      column(10,
+             h4("Disclaimer"),
+             h5("This software is in the public domain because it contains materials that originally came from the U.S. Geological Survey (USGS), an agency of the United States Department of Interior. For more information, see the official USGS copyright policy at http://www.usgs.gov/visual-id/credit_usgs.html#copyright
+                Although this software program has been used by the USGS, no warranty, expressed or implied, is made by the USGS or the U.S. Government as to the accuracy and functioning of the program and related program material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.
+                This software is provided 'AS IS.'"))
+
     )
     
   )
