e8002460381e616686a65c236ddc58247ec9d57f
Laura DeCicco <ldecicco@usgs.gov>
2017-04-10 13:51:08 -0500

    Fixes #169

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -181,7 +181,7 @@ body <- dashboardBody(
     ),
     column(11,
            h4("Disclaimer"),
-           h5("This software is in the public domain because it contains materials that originally came from the U.S. Geological Survey (USGS), an agency of the United States Department of Interior. For more information, see the official USGS copyright policy at http://www.usgs.gov/visual-id/credit_usgs.html#copyright
+           h5("This software is in the public domain because it contains materials that originally came from the U.S. Geological Survey (USGS), an agency of the United States Department of Interior. For more information, see the official USGS copyright policy at https://www.usgs.gov/visual-id/credit_usgs.html#copyright
               Although this software program has been used by the USGS, no warranty, expressed or implied, is made by the USGS or the U.S. Government as to the accuracy and functioning of the program and related program material nor shall the fact of distribution constitute any such warranty, and no responsibility is assumed by the USGS in connection therewith.
               This software is provided 'AS IS.'"))
   
