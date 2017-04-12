39f947813cc04dfbdb3de3724500a431ae63620e
Laura DeCicco <ldecicco@usgs.gov>
2017-04-12 12:37:21 -0500

    Table tests

--- tests/testthat/tests_summary.R
+++ tests/testthat/tests_summary.R
@@ -132,10 +132,75 @@ test_that("Map stuff functions", {
   expect_equal(length(mapDataList), 2)
 })
 
-# test_that("Test app", {
-#   testthat::skip_on_cran()
-#   
-#   explore_endpoints()
-#   
-#   stopApp(system.file('shiny', package='toxEval'))
-# })
+test_that("Table endpoint hits", {
+  testthat::skip_on_cran()
+
+  bt <- table_endpoint_hits(chemicalSummary, category = "Biological")
+  expect_type(bt, "list")
+  expect_true(all(names(bt$x$data) %in% c("endPoint","Nuclear Receptor","DNA Binding",
+                                  "Phosphatase","Steroid Hormone","Esterase")))
+  
+  expect_error(table_endpoint_hits(chemicalSummary, category = "Class"))
+  
+  ct <- table_endpoint_hits(chemicalSummary, category = "Chemical Class")
+  expect_type(ct, "list")
+  
+  expect_true(all(names(ct$x$data) %in% c("endPoint","Antioxidant",
+                                          "Detergent Metabolites","Herbicide",
+                                          "Plasticizer")))
+  
+  cht <- table_endpoint_hits(chemicalSummary, category = "Chemical")
+  expect_type(cht, "list")
+  
+  expect_true(all(names(cht$x$data) %in% c("endPoint","Bisphenol A",
+                                           "4-Nonylphenol, branched","Triphenyl phosphate",
+                                           "Metolachlor","Atrazine")))
+})
+
+test_that("table_tox_endpoint", {
+  testthat::skip_on_cran()
+  
+  bt <- table_tox_endpoint(chemicalSummary, category = "Biological")
+  expect_type(bt, "list")
+  expect_true("nSites" %in% names(bt$x$data))
+  
+  expect_error(table_tox_endpoint(chemicalSummary, category = "Class"))
+  
+  ct <- table_tox_endpoint(chemicalSummary, category = "Chemical Class")
+  expect_type(ct, "list")
+  
+  expect_true(all(names(ct$x$data) %in% c(" ","Nuclear Receptor","DNA Binding",
+                                          "Phosphatase","Esterase","Steroid Hormone",
+                                          "Zebrafish")))
+  
+  cht <- table_tox_endpoint(chemicalSummary, category = "Chemical")
+  expect_type(cht, "list")
+  
+  expect_true(all(names(cht$x$data) %in% c(" ","Nuclear Receptor","DNA Binding",
+                                           "Phosphatase","Esterase","Steroid Hormone",
+                                           "Zebrafish")))
+})
+
+
+test_that("table_tox_rank", {
+  testthat::skip_on_cran()
+  
+  bt <- table_tox_rank(chemicalSummary, category = "Biological")
+  expect_type(bt, "list")
+  expect_true("site" %in% names(bt$x$data))
+  
+  expect_error(table_tox_rank(chemicalSummary, category = "Class"))
+  
+  ct <- table_tox_rank(chemicalSummary, category = "Chemical Class")
+  expect_type(ct, "list")
+  
+  expect_true(all(c("site","Antioxidant maxEAR","Antioxidant freq",
+                    "Herbicide maxEAR","Herbicide freq",
+                    "Detergent Metabolites maxEAR","Detergent Metabolites freq") %in% names(ct$x$data)))
+  
+  cht <- table_tox_rank(chemicalSummary, category = "Chemical")
+  expect_type(cht, "list")
+  
+  expect_true(all(c("site","Bisphenol A maxEAR","Bisphenol A freq",
+                    "4-Nonylphenol, branched maxEAR") %in% names(cht$x$data)))
+})
