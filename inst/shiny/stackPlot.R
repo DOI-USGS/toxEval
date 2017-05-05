535f2a18ca9a824a5658ed1007362572badf3949
Laura DeCicco <ldecicco@usgs.gov>
2017-05-05 13:35:24 -0500

    Don't save unless asked

--- inst/shiny/stackPlot.R
+++ inst/shiny/stackPlot.R
@@ -1,19 +1,4 @@
-output$downloadStackPlot <- downloadHandler(
-  
-  filename = function() {
-    "stackPlot.png"
-  },
-  content = function(file) {
-    file.copy("stackPlot.png", file)
-  }
-)
-
-output$stackBarGroup <- renderPlot({
-  
-  validate(
-    need(!is.null(input$data), "Please select a data set")
-  )
-  
+stackBarGroup_create <- reactive({
   catType = as.numeric(input$radioMaxGroup)
   
   chemicalSummary <- chemicalSummary()
@@ -36,8 +21,28 @@ output$stackBarGroup <- renderPlot({
                                chem_site, 
                                category = c("Biological","Chemical","Chemical Class")[catType],
                                mean_logic = mean_logic)
-  
-  ggsave("stackPlot.png",upperPlot,bg = "transparent")
-  
-  upperPlot
+  return(upperPlot)
 })
+
+output$stackBarGroup <- renderPlot({
+  
+ validate(
+  need(!is.null(input$data), "Please select a data set")
+ )
+  
+ print(stackBarGroup_create())
+ 
+})
+
+output$downloadStackPlot <- downloadHandler(
+  
+  filename = "stackPlot.png",
+  
+  content = function(file) {
+    device <- function(..., width, height) {
+      grDevices::png(..., width = width, height = height,
+                     res = 300, units = "in")
+    }
+    ggsave(file, plot = stackBarGroup_create(), device = device)
+  }
+)
