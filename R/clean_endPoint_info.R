336ef66814601541a4912e693fe09557450f6197
Laura DeCicco <ldecicco@usgs.gov>
2018-05-08 14:10:30 -0500

    Link that doesn't make travis angry

--- R/clean_endPoint_info.R
+++ R/clean_endPoint_info.R
@@ -2,7 +2,7 @@
 #' 
 #' Define a subset of the ToxCast database for relevance to toxEval analyses. 
 #' Subsetting is done based upon methods defined by 
-#' \link[=https://pubs.acs.org/doi/10.1021/acs.est.7b01613]{Blackwell et al., 2017}. 
+#' \href[https://pubs.acs.org/doi/10.1021/acs.est.7b01613]{Blackwell et al., 2017}. 
 #' Specifically, this function removes endPoints that are ATG sources with 
 #' signal loss, and NVS with signal gain. Also, this function adds  additional 
 #' categories to intended_target_family and intended_target_family_sub as 
