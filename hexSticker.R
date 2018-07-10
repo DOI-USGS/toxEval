78422f7fb917067b70a38adc33e28df525bc571a
Laura DeCicco <ldecicco@usgs.gov>
2018-07-10 15:50:47 -0500

    Gotta sticker

---
+++ hexSticker.R
@@ -0,0 +1,13 @@
+library(hexSticker)
+
+outline = "#EAEBEF"
+background = "#000080" 
+sticker("./README_files/figure-markdown_github/tears.png",
+        package = "toxEval",
+        h_fill = background,
+        h_color = outline,
+        p_size = 25,
+        s_width = 0.35,
+        s_height = 0.35,
+        s_x = 1,
+        filename = "./README_files/figure-markdown_github/hexSticker.png")
