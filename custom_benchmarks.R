61932187e0f32c529fd5b59f897b16ee827e8f6e
Laura DeCicco <ldecicco@usgs.gov>
2018-07-11 15:52:35 -0500

    Removing old scripts

--- custom_benchmarks.R
+++
@@ -1,9 +0,0 @@
-path_to_excel <- "neonic_bench.xlsx"
-tox_list <- create_toxEval(path_to_excel)
-chemicalSummary <- get_chemical_summary(tox_list)
-
-plot_tox_boxplots(chemicalSummary, category = "Chemical")
-plot_tox_boxplots(chemicalSummary, category = "Chemical Class")
-
-plot_tox_boxplots(chemicalSummary, category = "Biological")
-
--- schedule_script.R
+++
@@ -1,9 +0,0 @@
-library(readxl)
-
-schedule <- "2524" #pesticide
-url.info <- paste0("http://nwqlqc/servlets_u/AnalyticalServicesGuide?srchStr=",schedule,"&srchType=sched&mCrit=exact&oFmt=xl")
-temp.path <- tempdir()
-temp.file <- file.path(temp.path,"AnalyticalServicesGuide.xls")
-download.file(url.info, destfile = temp.file, mode="wb")
-
-schedule.data <- read_excel(temp.file)
