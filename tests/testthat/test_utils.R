2682879f76ebce144fba257ef8ad37bcec308a8f
Laura DeCicco <ldecicco@usgs.gov>
2018-07-31 22:57:30 -0500

    More tests

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -23,12 +23,23 @@ test_that("Labels", {
   
   expect_equal(lab3[["caption"]], bquote(italic("i = chemicals in a specified grouping, j = samples, k = sites")))
   expect_equal(lab3[["y_label"]], bquote(italic("mean") * group("[", italic(max) * group("(",EAR["[" * i * "]"], ")")["[" * j * "]"], "]")["[" * k * "]"]))
-                                                                                      
+               
+  nums1 <- toxEval:::fancyNumbers(c(10, 100, 1000, 10000))
+  expect_equal(nums1[[1]], 10)
+  expect_equal(as.character(nums1[[4]]), c("^","10","4"))                                                           
+  
+  nums2 <- toxEval:::fancyNumbers2(c(10, 100, 1000, 10000))
+  expect_equal(nums2, c("10","100","1000","10000"))                                                           
+  
+  nums3 <- toxEval:::prettyLogs(c(0.01,0.5,1,20,300))
+  expect_equal(nums3, c(0.01, 0.1, 1, 10, 100, 1000))
 })
 
-# NEED:
-# fancyNumbers2
-# fancyNumbers
-# prettyLogs
+
+test_that("Complete", {
+  testthat::skip_on_cran()
+  
+})
+
 # get_complete_set_category
 # get_complete_set
