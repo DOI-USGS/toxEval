c71e4db02da55f2f953d48eae98b253b295673cf
Laura DeCicco <ldecicco@usgs.gov>
2018-07-31 23:57:05 -0500

    Take off disclaimer!

--- R/toxEval.R
+++ R/toxEval.R
@@ -1,15 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    paste(strwrap('This information is preliminary or provisional and
-is subject to revision. It is being provided to meet
-the need for timely best science. The information
-has not received final approval by the U.S. Geological
-Survey (USGS) and is provided on the condition that
-neither the USGS nor the U.S. Government shall be held
-liable for any damages resulting from the authorized
-or unauthorized use of the information.
-
-USGS Research Package: 
+    paste(strwrap('USGS Research Package: 
 https://owi.usgs.gov/R/packages.html#research'),
       collapse='\n'))
 }
