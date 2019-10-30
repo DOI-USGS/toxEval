bf08939205fa089dbba2c4c6e04f5f63a657ad49
Laura <ldecicco@usgs.gov>
2019-10-30 15:38:37 -0500

    Side-by-side structure setup

--- R/side_by_side.R
+++ R/side_by_side.R
@@ -1,14 +1,24 @@
 #' Prepare boxplot data
 #' 
-#' A set of functions to prepare the data for boxplots. 
+#' A set of functions to prepare the data for boxplots. Often, these
+#' functions are used within the plotting functions. They are exported however
+#' to allow custom graphs to be created. 
+#' 
+#' The function side_by_side_data will combine two data frames,
+#' either the output of \code{\link{get_chemical_summary}} or \code{\link{graph_chem_data}},
+#' into a single data frame. The important work here is that the chemicals
+#' and classes factor levels are ordered primarily based on "gd_left", but
+#' include "gd_right" when the contents are mismatched.
 #' 
 #' @export
 #' @rdname graph_data_prep
 #' 
-#' @param gd_left data.frame
-#' @param gd_right data.frame
-#' @param left_title Character
-#' @param right_title Character
+#' @param gd_left Data frame that must include the columns chnm, Class, and either EAR or meanEAR.
+#' @param gd_right Data frame that must include the columns chnm, Class, and either EAR or meanEAR.
+#' @param left_title Character that will be associated with the "gd_left" data 
+#' frame in a column named "guide_side". 
+#' @param right_title Character that will be associated with the "gd_right" data 
+#' frame in a column named "guide_side". 
 #' 
 #' 
 side_by_side_data <- function(gd_left, 
