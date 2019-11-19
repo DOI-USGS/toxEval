d2f613b39fcc4fe7a5300e7a55d92f870addee41
Laura <ldecicco@usgs.gov>
2019-11-19 10:10:23 -0600

    Better messaging format

--- R/toxEval.R
+++ R/toxEval.R
@@ -1,8 +1,8 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage(
-    paste(strwrap('USGS Research Package: 
+    paste(strwrap('USGS Research Package:
 https://owi.usgs.gov/R/packages.html#research
-ToxCast database: version 3'),
+ToxCast database: version 3', width = 40),
       collapse='\n'))
 }
 
