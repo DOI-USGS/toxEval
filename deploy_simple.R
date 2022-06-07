c61cb262f73a61f1c3ba08d864741180201d0e81
DeCicco <ldecicco@usgs.gov>
2022-06-07 11:52:42 -0500

    Try a new deploy

---
+++ deploy_simple.R
@@ -0,0 +1,17 @@
+library(connectapi)
+client <- connect(server = Sys.getenv("CONNECT_SERVER"),
+                  api_key = Sys.getenv("CONNECT_API_KEY"))
+
+rsconnect::writeManifest(appDir = "./docs")
+bundle <- bundle_dir("./docs")
+
+content <- client %>% 
+  deploy(bundle, name = "toxEval_docs") %>% 
+  poll_task()
+
+rsconnect::writeManifest(appDir = "./inst/shiny")
+bundle <- bundle_dir("./inst/shiny")
+
+content <- client %>% 
+  deploy(bundle, name = "toxEval") %>% 
+  poll_task()
