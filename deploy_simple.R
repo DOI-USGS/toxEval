eb192b4303add63da18463e3460bd629af62c353
DeCicco <ldecicco@usgs.gov>
2022-06-15 09:11:23 -0500

    Got to get the logo to reference folder

--- deploy_simple.R
+++ deploy_simple.R
@@ -2,6 +2,9 @@ library(connectapi)
 client <- connect(server = Sys.getenv("CONNECT_SERVER"),
                   api_key = Sys.getenv("CONNECT_API_KEY"))
 
+file.copy(from = "./docs/articles/logo.png", 
+          to = "./docs/reference/logo.png")
+
 rsconnect::writeManifest(appDir = "./docs")
 bundle <- bundle_dir("./docs")
 
@@ -9,9 +12,9 @@ content <- client %>%
   deploy(bundle, name = "toxEval_docs") %>% 
   poll_task()
 
-# rsconnect::writeManifest(appDir = "./inst/shiny")
-# bundle <- bundle_dir("./inst/shiny")
-# 
-# content <- client %>% 
-#   deploy(bundle, name = "toxEval") %>% 
-#   poll_task()
+rsconnect::writeManifest(appDir = "./inst/shiny")
+bundle <- bundle_dir("./inst/shiny")
+
+content <- client %>%
+  deploy(bundle, name = "toxEval") %>%
+  poll_task()
