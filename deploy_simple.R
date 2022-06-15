33a054a971400de8001c35e838f1e9caf98df601
DeCicco <ldecicco@usgs.gov>
2022-06-15 09:47:45 -0500

    just won't work...I think it's if the package I'm currently using doesn't match what is on Rpkg manager

--- deploy_simple.R
+++ deploy_simple.R
@@ -11,10 +11,10 @@ bundle <- bundle_dir("./docs")
 content <- client %>% 
   deploy(bundle, name = "toxEval_docs") %>% 
   poll_task()
-
-rsconnect::writeManifest(appDir = "./inst/shiny")
-bundle <- bundle_dir("./inst/shiny")
-
-content <- client %>%
-  deploy(bundle, name = "toxEval") %>%
-  poll_task()
+# 
+# rsconnect::writeManifest(appDir = "./inst/shiny")
+# bundle <- bundle_dir("./inst/shiny")
+# 
+# content <- client %>%
+#   deploy(bundle, name = "toxEval") %>%
+#   poll_task()
